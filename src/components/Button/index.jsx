c5f25b204baa93654ed669d7f2cabac1b2ecf072
Nikita Nikolov <nnikolov-post@gmail.com>
2020-04-05 16:06:34 +0100

    Adde dthumbnail & blue buttons

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styles from './button.module.scss'
 
-const Button = ({children, invert}) => (
-    <div className={invert ? styles.Invert : styles.Button}>
+const Button = ({children, invert, blue}) => (
+    <div className={`${invert ? styles.Invert : styles.Button} ${blue && styles.Blue}`}>
         {children}
     </div>
 )
