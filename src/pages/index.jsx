f15591d5c0beb07c2eb50713a2f7586faaf92bbd
Will <you@example.com>
2020-04-05 19:12:48 +0100

    grid improved - not finished

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,30 +33,30 @@ const Index = () => {
 					<h1>Happy, Healthy &<br />Inspired.</h1>
 					<img className="image" src={heroImage}></img>
 					<div className="buttons are-small">
-						<div className="button is-primary"><Button>LogIn</Button></div>
-						<div className="button is-primary is-outlined"><Button invert>Create Account</Button></div>
+						<button className="button is-primary">LogIn</button>
+						<button className="button is-primary is-outlined">Create Account</button>
 					</div>
 					<p>You are never alone. Happy, healthy,<br /> connected Wellness with WellNest.</p>
 				</div>
-				{/* <div className="video-grid">
-					<div class="level">
-						<div class=" buttons are-small" >
-							<span className="button is-info"><Button>Open Cam</Button></span>
-							<span className="button is-info"><Button>Open Mic</Button></span>
-							<span className="button is-info is-outlined"><Button>Join Room</Button></span>
-							<span className="button is-info is-outlined"><Button>Hang Up</Button></span>
-						</div>
+			</div>
+			<div className="video-grid">
+				<div class="level">
+					<div class=" buttons are-small" >
+						<button className="button is-info">Open Cam</button>
+						<button className="button is-info">Open Mic</button>
+						<button className="button is-info is-outlined">Join Room</button>
+						<button className="button is-info is-outlined">Hang Up</button>
 					</div>
-					<div className="tile is-ancestor is-12">
-						<Thumbnail className="tile" image={heroImage} />
-						<Thumbnail className="tile" image={heroImage} />
-						<Thumbnail className="tile" image={heroImage} />
-						<Thumbnail className="tile" image={heroImage} />
-						<Thumbnail className="tile" image={heroImage} />
-						<Thumbnail className="tile" image={heroImage} />
+				</div>
+				<div className="tile is-ancestor is-12">
+					<div className="thumbnail tile"><Thumbnail image={heroImage} /></div>
+					<div className="thumbnail tile"><Thumbnail image={heroImage} /></div>
+					<div className="thumbnail tile"><Thumbnail image={heroImage} /></div>
+					<div className="thumbnail tile"><Thumbnail image={heroImage} /></div>
+					<div className="thumbnail tile"><Thumbnail image={heroImage} /></div>
+					<div className="thumbnail tile"><Thumbnail image={heroImage} /></div>
 
-					</div>
-				</div> */}
+				</div>
 			</div>
 		</>
 	)
